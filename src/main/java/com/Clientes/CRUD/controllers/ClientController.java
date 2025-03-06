4edff32eaa854137dac194c2e887c084e696fc1d
DenielGuilhen <deniel.guilhen22@gmail.com>
2025-03-06 20:33:44 -0300

    Get by Id e FindAll

--- src/main/java/com/Clientes/CRUD/controllers/ClientController.java
+++ src/main/java/com/Clientes/CRUD/controllers/ClientController.java
@@ -1,5 +1,13 @@
 package com.Clientes.CRUD.controllers;
 
+import com.Clientes.CRUD.dto.ClientDTO;
+import com.Clientes.CRUD.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/clients")
 public class ClientController {
 
+    @Autowired
+    private ClientService clientService;
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = clientService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = clientService.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
 
 }
